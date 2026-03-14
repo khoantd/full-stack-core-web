2006dc4f1e8ae0a4a239793e7526caaeafef6b41
Hau Pham <phaucau2311@gmail.com>
2026-03-15 06:07:21 +0700

    chang main.ts to ip v4

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,5 @@
+import { setDefaultResultOrder } from 'node:dns';
+setDefaultResultOrder('ipv4first'); // Ép toàn bộ app dùng IPv4 trước
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
